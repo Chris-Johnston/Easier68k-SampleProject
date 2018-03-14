6c1faa5cc47fcae973002b5d6d4c4c722f649e11
Chris Johnston <githubchrisjohnston@gmail.com>
2018-03-14 01:43:33 -0700

    initial commit

---
+++ sample_app.py
@@ -0,0 +1,24 @@
+"""
+Easier68k-SampleProject
+
+This is a test application as an example of using easier68k as an external python package.
+
+Further examples will come later.
+"""
+
+# install using pip install -r requirements.txt
+# you may need to use the --upgrade flag
+import easier68k
+from easier68k.core.enum.register import Register
+
+if __name__ == '__main__':
+
+    # create a simulator object
+    sim = easier68k.simulator.m68k.M68K()
+
+    # set register D1 value to 123
+    sim.set_register_value(Register.D1, 123)
+
+    assert sim.get_register_value(Register.D1) == 123
+
+    print('done w/o errors')
